9b631c176b186a1f9831b2c31e5e5f68afd27313
Jaucordi <jaucordi@gmail.com>
2022-11-19 00:00:21 +0100

    Adding `Router` React component
    - Pages are lazy-loaded

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,3 +1,12 @@
+import {Layout}                                      from "./components/layout/Layout";
+import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
+import React                                         from "react";
+
+const ErrorPage = React.lazy(() => import("./pages/error/ErrorPage"));
+const HomePage = React.lazy(() => import("./pages/home/HomePage"));
+const LogementPage = React.lazy(() => import("./pages/logement/LogementPage"));
+const AboutPage = React.lazy(() => import("./pages/about/AboutPage"));
+
 export enum Routes {
   ROOT = "/",
 
@@ -6,3 +15,37 @@ export enum Routes {
   LOGEMENT = "logement/:id",
   ABOUT = "about",
 }
+
+export const Root = () => {
+  return (
+      <Layout>
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <Outlet />
+        </React.Suspense>
+      </Layout>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: Routes.ROOT,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: Routes.HOME,
+        element: <HomePage />,
+      },
+      {
+        path: Routes.LOGEMENT,
+        element: <LogementPage />,
+      },
+      {
+        path: Routes.ABOUT,
+        element: <AboutPage />,
+      }
+    ],
+  }
+]);
+
+export const Router = () => (<RouterProvider router={router} />);
